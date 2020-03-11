7b21df5d77d48b3a24770e92c685d8e4629c607f
jules <jbi.octave@gmail.com>
2020-03-11 16:41:55 +0000

    11 March

--- application/__init__.py
+++ application/__init__.py
@@ -1,6 +1,25 @@
-# import Flask class from the flask module
 from flask import Flask
-# create a new instance of Flask and store it in app 
+from flask_sqlalchemy import SQLAlchemy
+
+from os import getenv
+
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
 app = Flask(__name__)
-# import the ./application/routes.py file
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+bcrypt = Bcrypt(app)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
+
+db = SQLAlchemy(app)
+
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
+
 from application import routes
+
+
+
--- application/routes.py
+++ application/routes.py
@@ -1,28 +1,72 @@
 # import render_template function from the flask module
-from flask import render_template
-# import the app object from the ./application/__init__.py
-from application import app
-# define routes for / & /home, this function will be called when these are accessed
-blogData = [
-    {  
-        "name": {"first":"John", "last":"Doe"},
-        "title":"First Post",
-        "content":"This is some blog data for Flask lectures"
-    },
-    {   
-        "name": {"first":"Jane", "last":"Doe"},
-        "title":"Second Post",
-        "content":"This is even more blog data for Flask lectures"
-    }
-]
+from flask import render_template, redirect, url_for,request
+from application import app, db, bcrypt
+from application.models import Posts, Users
+from application.forms import PostForm, RegistrationForm, LoginForm
+from flask_login import login_user, current_user, logout_user, login_required
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    form = PostForm()
+    if form.validate_on_submit():
+        postData = Posts(
+            first_name = form.first_name.data,
+            last_name = form.last_name.data,
+            title = form.title.data,
+            content = form.content.data
+        )
+
+        db.session.add(postData)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+
+    else:
+        print(form.errors)
+
+    return render_template('post.html', title='Post', form=form)
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html', title='Home', posts=blogData)
+    postData = Posts.query.first()
+    return render_template('home.html', title='Home', post=postData)
 @app.route('/about')
 def about():
     return render_template('about.html', title='about')
-@app.route('/register')
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template('register.html', title='about')
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        hash_pw = bcrypt.generate_password_hash(form.password.data)
+
+        user = Users(email=form.email.data, password=hash_pw)
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('post'))
+    return render_template('register.html', title='Register', form=form)
+
+@app.route("/login", methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user=Users.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            if next_page:
+                return redirect(next_page)
+            else:
+                return redirect(url_for('home'))
+    return render_template('login.html', title='Login', form=form)
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
